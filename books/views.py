2ea3e28cf85b3b9bd69bb9e20d51a4f57db985b6
loganatkinson <logan.atkinson@gmail.com>
2021-06-22 14:07:17 -0400

    added registration

--- books/views.py
+++ books/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 
 
 # Create your views here.
+def homepage(request):
+    return render(request, "books/homepage.html")
+
+@login_required
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("created_at")
     return render(request, "books/list_books.html",
     {"books": books})
--- django_freeshelf/urls.py
+++ django_freeshelf/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from books import views as book_views
+
 
 urlpatterns = [
+    path("", book_views.homepage, name="home"),
+    path("accounts/", include('registration.backends.default.urls')),
     path("admin/", admin.site.urls),
-    path('', include('books.urls')),
 ]
